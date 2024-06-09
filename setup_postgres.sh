fab8d29b7b0f172aa7693bd113f70af56c88453a
0509pooja <149455505+0509pooja@users.noreply.github.com>
2024-06-09 08:53:56 +0530

    Update setup_postgres.sh

--- setup_postgres.sh
+++ setup_postgres.sh
@@ -1,21 +1,48 @@
 #!/bin/bash
 
-# Update and install PostgreSQL
-sudo apt-get update
-sudo apt-get install -y postgresql postgresql-contrib
+# Install PostgreSQL
+sudo apt update
+sudo apt install postgresql postgresql-contrib
 
-# Switch to the postgres user and create a new database and user
-sudo -i -u postgres psql <<EOF
-CREATE DATABASE wp;
-CREATE USER poojauser WITH ENCRYPTED PASSWORD 'poojapassword';
-GRANT ALL PRIVILEGES ON DATABASE wp TO poojauser;
+# Start PostgreSQL service
+sudo systemctl start postgresql.service
+
+# Update postgresql.conf to listen on all IP addresses
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/16/main/postgresql.conf
+
+# Update pg_hba.conf to allow external connections
+echo "host    all             all             0.0.0.0/0               md5" | sudo tee -a /etc/postgresql/16/main/pg_hba.conf
+
+# Restart PostgreSQL service
+sudo systemctl restart postgresql.service
+
+# Create a database and user for the Flask app
+sudo -u postgres psql -c "CREATE DATABASE $DB_NAME;"
+sudo -u postgres psql -c "CREATE USER $DB_USERNAME WITH PASSWORD '$DB_PASSWORD';"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USERNAME;"
+
+#init_db.py script
+cat << EOF > init_db.py
+import os
+import psycopg2
+
+conn = psycopg2.connect(
+        host=os.environ['DB_HOST'],
+        database=os.environ['DB_NAME'],
+        user=os.environ['DB_USERNAME'],
+        password=os.environ['DB_PASSWORD'])
+
+cur = conn.cursor()
+cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
+            'title varchar (150) NOT NULL,'
+            'author varchar (50) NOT NULL,'
+            'pages_num integer NOT NULL,'
+            'review text,'
+            'date_added date DEFAULT CURRENT_TIMESTAMP);')
+conn.commit()
+cur.close()
+conn.close()
 EOF
 
-# Allow external connections to PostgreSQL
-sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/12/main/postgresql.conf
-echo "host    all             all             0.0.0.0/0               md5" | sudo tee -a /etc/postgresql/12/main/pg_hba.conf
+python init_db.py
 
-# Restart PostgreSQL to apply changes
-sudo systemctl restart postgresql
-
-echo "PostgreSQL setup complete."
